a5176f2a173dcf2de583851a7b1def920843fd49
kaliyappan <yappank17@gmail.com>
2024-04-24 16:40:26 +0530

    commit

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
-// const db_connection = require('./database/db_connection');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded());
 app.use(express.static( 'public'));
 // app.use(db_connection);
 
 
-
+const port = process.env.PORT || 3000;
 app.listen(port, (err, res) => {
     console.log(`listening on port: ${port} >  http://localhost:${port}`);
 });
